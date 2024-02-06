5478e66b87d16eb8156e6934cf64bda0c1e39ca9
J3alou5 <145442169+J3alou5@users.noreply.github.com>
2024-02-06 10:25:19 +0200

    Created form request responce with API data

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express()
 const path = require('path')
 const fetch = require('node-fetch')
 
+app.use(express.urlencoded({ extended: true }));
+
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
@@ -24,8 +26,23 @@ res.render('index', {
     temp: temp
 })
 })
-
 })
-
-
+app.post('/', function(req, res) {
+    let city = req.body.cityname;
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+    .then(response => {
+      return response.json()
+    })
+    .then((data) => {
+      let description = data.weather[0].description;
+      let city = data.name;
+      let temp = Math.round(parseFloat(data.main.temp) - 273.15)
+      res.render('index', {
+        description: description,
+        city: city,
+        temp: temp
+      });
+    })
+  })
+  
 app.listen(3000)
